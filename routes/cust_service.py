691c9985804153c371b95294c9dd6320f7b253b2
SamuelAlvianM <oensam07@gmail.com>
2025-03-13 11:12:26 +0700

    1

--- routes/cust_service.py
+++ routes/cust_service.py
@@ -16,3 +16,18 @@ def get_cust_by_id(customer_id):
     if not customer:
         return jsonify({"message": "Coffee not found"}), 404
     return jsonify(customer), 200
+
+@cust_blueprint.route("/coffee/<int:customer_id>", methods=["PUT"])
+def update_coffee(customer_id):
+    if customer_id not in customer_db:
+        return jsonify({"error": "Coffee not found"}), 404
+
+    data = request.json
+    customer_db[customer_id].update({
+        "name": data.get("name", customer_db[customer_id]["name"]),
+        "product": data.get("product", customer_db[customer_id]["product"]),
+        "ingredients": data.get("ingredients", customer_db[customer_id]["ingredients"]),
+        "sold": data.get("sold", customer_db[customer_id]["sold"]),
+    })
+
+    return jsonify(customer_db[customer_id]), 200
